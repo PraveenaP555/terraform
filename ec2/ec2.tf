b890006b4240b578e3aa9e7eb00b1f29e4245989
PraveenaP <praveena.chowdhary28@gmail.com>
2024-09-20 14:24:50 +0530

    expense

--- ec2/ec2.tf
+++ ec2/ec2.tf
@@ -1,3 +1,12 @@
+resource "aws_instance" "terraform" {
+    ami = "ami-09c813fb71547fc4f"
+    instance_type = "t3.micro"
+    vpc_security_group_ids = [aws_security_group.allow_ssh_terraform.id]
+    tags = {
+        Name = "terraform"
+    }
+}
+
 resource "aws_security_group" "allow_ssh_terraform" {
     name = "allow_sshh"
     description = "Allow port number 22 for ssh access"
@@ -15,6 +24,10 @@ resource "aws_security_group" "allow_ssh_terraform" {
         cidr_blocks      = ["0.0.0.0/0"]
         ipv6_cidr_blocks = ["::/0"]
     }
+    tags = {
+        Name = "allow_sshh"
+    }
 }
+
 
 
